ac207cd36e283e1aaf433004fd33a5219177b377
Lucasemmanuel96 <98363713+Lucasemmanuel96@users.noreply.github.com>
2023-09-05 21:23:14 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+function toggleElement() {
+  var elemento = document.getElementById("mi-elemento");
+  if (elemento.style.display === "block") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "block";
+  }
+}
